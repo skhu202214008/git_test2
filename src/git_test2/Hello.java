4c05450de3f5739456572f497e5147aae2d42cd0
Heo, Jinwoo <skhu202214008@gmail.com>
2026-04-08 09:17:24 +0900

    화면 출력방법 수정

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -1,8 +1,8 @@
 package git_test2;
-
 public class Hello {
-	String message="안녕하세요";
-	public void say() {
-		System.out.println(message);
-	}
+ 
+ String message = "안녕하세요";
+ public String say() {
+ return message;
+ }
 }
